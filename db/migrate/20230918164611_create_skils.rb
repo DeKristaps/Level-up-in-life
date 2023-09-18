f1d76c7ec230645980cceeeccd0c4db849752a24
DeKristaps <dekristaps@gmail.com>
2023-09-18 20:23:27 +0300

    Adds relations between skill and user and fixes mistake with skill table naming and params

--- db/migrate/20230918164611_create_skils.rb
+++ db/migrate/20230918164611_create_skils.rb
@@ -1,12 +1,13 @@
 class CreateSkils < ActiveRecord::Migration[7.0]
   def change
-    create_table :skils do |t|
-      t.string :strength
-      t.string :dexterity 
-      t.string :constitution
-      t.string :intelligence 
-      t.string :wisdom
-      t.string :charisma
+    create_table :skills do |t|
+      t.references :user, foreign_key: true
+      t.integer :strength, default: 0
+      t.integer :dexterity, default: 0
+      t.integer :constitution, default: 0
+      t.integer :intelligence, default: 0
+      t.integer :wisdom, default: 0
+      t.integer :charisma, default: 0
       t.timestamps
     end
   end
