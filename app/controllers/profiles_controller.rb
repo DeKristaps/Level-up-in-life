aa6d20e8e837a266ea40a1c1fa92a0edfce453ed
DeKristaps <dekristaps@gmail.com>
2023-09-20 21:12:38 +0300

    Adds stats to profile window

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,13 @@ class ProfilesController < ApplicationController
 
   def show
     @user = User.find_by(id: current_user.id)
+    @skill = @user.skill
+    @stats = get_skills
+  end
+
+  private
+
+  def get_skills
+    @skill.attributes.slice('strength', 'dexterity', 'constitution', 'intelligence', 'wisdom', 'charisma')
   end
 end
