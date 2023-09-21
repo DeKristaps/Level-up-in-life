23c8e186b355a47f197ba0cdd94be8d09d425a85
DeKristaps <dekristaps@gmail.com>
2023-09-21 21:47:36 +0300

    Adds 2 default users to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.new(email: 'dekristaps@gmail.com', nickname: 'LinuPuika')
+user.password = 'admin123'
+user.password_confirmation = 'admin123'
+user.save
+
+user2 = User.new(email: 'test@example.com', nickname: 'Testy Testerson')
+user2.password = 'admin123'
+user2.password_confirmation = 'admin123'
+user2.save
