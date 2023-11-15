8ba67c4dbbfa5a4aa5e7292632f663992a4f8afa
DeKristaps <dekristaps@gmail.com>
2023-11-15 21:05:25 +0200

    Fixes issue with css not updating while in development

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,15 @@ users.each { | user |
 
 
 quests = []
-quests << Quest.new(title: 'Do the dishes', description: 'You have to wash all the dishes in the sink', xp_for_stat: 'constitution', xp_given: 15)
-quests << Quest.new(title: 'Worckout', description: 'Just lift something hevy for about 20 min', xp_for_stat: 'strength', xp_given: 15)
+quests << Quest.new(title: 'Do the dishes', description: 'You have to wash all the dishes in the sink', xp_for_stat: 'dexterity', xp_given: 15)
+quests << Quest.new(title: 'Workout', description: 'Just lift something hevy for about 20 min', xp_for_stat: 'strength', xp_given: 15)
+quests << Quest.new(title: 'Go for a run', description: 'Go for a 20 min run', xp_for_stat: 'constitution', xp_given: 15)
+quests << Quest.new(title: 'Read a book', description: 'Finish a chaper of a book', xp_for_stat: 'intelligence', xp_given: 15)
+quests << Quest.new(title: 'Finish a book', description: 'Finish a entire book', xp_for_stat: 'intelligence', xp_given: 80)
+quests << Quest.new(title: 'Talk to some people', description: 'Go out and talk to some people', xp_for_stat: 'charisma', xp_given: 15)
+quests << Quest.new(title: 'Leg day', description: 'Do a leg workout', xp_for_stat: 'strength', xp_given: 15)
+quests << Quest.new(title: 'Meditate', description: 'Do a 20 min meditation', xp_for_stat: 'wisdom', xp_given: 15)
+quests << Quest.new(title: 'Learn guitar', description: 'Do some guitar exercises', xp_for_stat: 'dexterity', xp_given: 15)
 
 quests.each { |quest| 
   quest.save unless Quest.find_by(title: quest.title)
